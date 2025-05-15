9803b8192f89a94197c2b22b94e401b9f0d1b222
Madan Kumar <madankumar.p@navabrindit.com>
2025-05-15 21:52:41 +0530

    homepage design

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,19 @@
+import OurService from "../../components/home/ourService";
+import OurAdvantage from "../../components/home/ourAdvantage";
+import TestimonialsSection from "../../components/home/testimonial";
+import Banner from "../../components/home/banner";
+import LocationMap from "../../components/home/locationMap";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <OurService />
+      <OurAdvantage />
+      <TestimonialsSection />
+      <LocationMap />
+    </>
+  );
+};
+
+export default Home;
